233e9e5447a45ebb4d2e10f02d341895c258ab13
Kabirov7 <arturkabirov16@gmail.com>
2020-09-23 14:29:25 +0600

    Updates

---
+++ precache-manifest.01102149fceec255a0b46dc74d38d908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a0c1876dd0071405c998ca73e18616",
+    "url": "/kloop-table/index.html"
+  },
+  {
+    "revision": "b3e4428fafd6d31cf998",
+    "url": "/kloop-table/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1822b6977877690336b4",
+    "url": "/kloop-table/static/js/2.1da138e5.chunk.js"
+  },
+  {
+    "revision": "e1a2b590289b8b0a443a0c2fb2738ca4",
+    "url": "/kloop-table/static/js/2.1da138e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e4428fafd6d31cf998",
+    "url": "/kloop-table/static/js/main.baceb7cf.chunk.js"
+  },
+  {
+    "revision": "2d3882f35a7fc224978a",
+    "url": "/kloop-table/static/js/runtime-main.965dc7b2.js"
+  }
+]);
